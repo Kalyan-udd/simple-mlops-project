ee594f21a60092e6f63c7c16853cc943897ca7e9
Kalyan_udd <kalyansaiuddagiri22@gmail.com>
2026-06-23 21:51:25 +0530

    completeing the whole code.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from fastapi.responses import FileResponse
 import joblib
 from research.Model import filepath
 from schemas import BankNoteModel
-import pandas as pd
+from ml_project.logger import logger
+from pandas import DataFrame
+
 
 app = FastAPI()
 
@@ -11,29 +13,30 @@ model = joblib.load(filename=filepath)
 
 @app.get("/")
 def home():
-
+    logger.info("Home page is active...!")
     return FileResponse("templates/index.html")
 
 @app.get("/tools")
 def tools():
+    logger.info("tools page is active..!")
     return FileResponse("templates/Predict.html")
-
-       
-
 
 @app.post("/predict")
 async def ModelPrediction(data: BankNoteModel):
-    input_df = pd.DataFrame([data.model_dump()])
-    #model_dump() attribute converts the data into a python dictionary instead of something that could only be read by pydantic
-    prediction = model.predict(input_df)
-    if prediction[0] == 1:
-        result = "Fake Note"
-    else:
-        result = "Not a Fake Note"
-
-    return {
-        "status": "success",
-        "prediction": prediction[0],
-        "result": result
-    }
+    try:
+        input_df = DataFrame([data.model_dump()])
+        #model_dump() attribute converts the data into a python dictionary instead of something that could only be read by pydantic
+        prediction = model.predict(input_df)
+        if prediction[0] == 1:
+            result = "Fake Note"
+        else:
+            result = "Not a Fake Note"
+        logger.info("server responded successfully")
+        return {
+            "status": "success",
+            "prediction": int(prediction[0]),
+            "result": result
+        }
+    except Exception as e:
+        logger.info(f"Error occurred - {e}")
 
--- src/ml_project/logger.py
+++ src/ml_project/logger.py
@@ -6,13 +6,19 @@ import sys
 file_dir = "logs"
 filepath = os.path.join(file_dir, "logging.log")
 
-logger = logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s]: [%(filename)s: %(lineno)d] - %(message)s",
-    handlers=[
-        logging.FileHandler(filepath),
-        logging.StreamHandler(sys.stdout)
-    ]
-)
-
 logger = logging.getLogger("SimpleMLOpsProjectLogger")
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("[%(asctime)s]: [%(filename)s: %(lineno)d] - %(message)s")
+
+if not logger.handlers:
+    file_handler = logging.FileHandler(filepath)
+    file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    logger.propagate = False
